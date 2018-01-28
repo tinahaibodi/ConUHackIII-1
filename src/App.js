9d56fd8630dafa7d92b0966ea4ec5dd172f0c816
amyxlu <x84lu@edu.uwaterloo.ca>
2018-01-28 01:09:32 -0500

    webcam fixed

--- src/App.js
+++ src/App.js
@@ -9,25 +9,48 @@ class WebcamCapture extends React.Component {
     this.state = {
       message: ''
     };
+    this.count = 0;
+    this.pictures = [];
+    this.cap = this.cap.bind(this);
+    this.capture = this.capture.bind(this);
   }
+
   setRef = (webcam) => {
     this.webcam = webcam;
   }
 
+
   capture = () => {
-    const imageSrc = this.webcam.getScreenshot();
+    var imageSrc = this.webcam.getScreenshot();
+    this.pictures.push(imageSrc);
     // var data = function to convert imageSrc to the format
     // var data;
     // $.post("demo_test.asp", data, function(data, status){ // $ is a jquery object
     //     alert("Data: " + data + "\nStatus: " + status);
     // });
-    this.setState({message: 'I took the picture'});
-  };
+    console.log(imageSrc)
+    this.setState({message: 'I took the picture' + String(this.count)});
+    this.count += 1;
+
+    if (this.count == 10) {
+      clearInterval(this.nIntervalId); 
+      this.count = 0;
+   //  $.post("demo_test.asp", this.pictures, function(data, status){ // $ is a jquery object
+   //     alert("Data: " + data + "\nStatus: " + status);
+  //    });
+      this.pictures = [];
+    }  
+
+  }
+
+  cap() {
+    this.nIntervalId = setInterval(this.capture, 500); 
+  }
 
   render() {
     return (
       <div>
-        {this.state.message}
+      {this.state.message}
         <Webcam
           audio={false}
           height={350}
@@ -35,8 +58,9 @@ class WebcamCapture extends React.Component {
           screenshotFormat="image/jpeg"
           width={350}
         />
-        <button onClick={this.capture}>Capture photo</button>
+        <button onClick={this.cap}>Capture photo</button>
       </div> 
+      )
   }
 }
 
